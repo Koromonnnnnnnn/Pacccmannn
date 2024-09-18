078b6650cab72e933ca718e132ce3c5685a036b6
Koromonnnnnnnn <764363@dpsk12.net>
2024-09-18 10:19:17 -0600

    .

--- Pacccmannn/main.cpp
+++ Pacccmannn/main.cpp
@@ -1,10 +1,20 @@
 #include<SFML/Graphics.hpp>
+#include "Player.h"
 using namespace sf;
 using namespace std;
 
+void handleInput(Player& player) {
+	if (Keyboard::isKeyPressed(Keyboard::Left)) player.move(-1, 0);
+	if (Keyboard::isKeyPressed(Keyboard::Right)) player.move(1, 0);
+	if (Keyboard::isKeyPressed(Keyboard::Up)) player.move(0, -1);
+	if (Keyboard::isKeyPressed(Keyboard::Down)) player.move(0, 1);
+}
+
+Player player;
+
 int main() {
 
-	RenderWindow window(VideoMode(800, 600), "Pacman Game", Style::Fullscreen); window.setFramerateLimit(60);
+	RenderWindow window(VideoMode(800, 600), "Pacman Game", Style::Close); window.setFramerateLimit(60);
 
 	Event event;
 
@@ -16,8 +26,10 @@ int main() {
 
 		}
 
-		window.clear((Color::Black));
+		handleInput(player);
 
+
+		window.clear((Color::Black));
 
 
 		window.display();
